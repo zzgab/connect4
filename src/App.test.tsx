56c00ef2ef9596733f5fe204afe22ac73beb05b1
GabrielZ <gabriel.zerbib@walkme.com>
2022-03-24 17:15:10 +0200

    fini v2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {getAllByTestId, render, screen} from "@testing-library/react";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('connect4',  () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('renders game board', () => {
+    expect(screen.getByTestId('board')).toBeInTheDocument();
+  });
+
+  it('has 7 columns', () => {
+    expect(screen.getAllByTestId('column').length).toBe(7);
+  });
+
+  it('has 6 slots in every column', () => {
+    const columns = screen.getAllByTestId('column');
+    columns.forEach(column => {
+      const slots = getAllByTestId(column, 'slot');
+      expect(slots.length).toBe(6);
+    });
+  });
+
+  it('has a NextPlayer visual, starting Yellow', () => {
+    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+  });
+
+  it('changes current player when click in free place', () => {
+    screen.getAllByTestId('column')[0].click();
+    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-R');
+    expect(screen.getByTestId('nextPlayer')).not.toHaveClass('Player-Y');
+
+    screen.getAllByTestId('column')[0].click();
+    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+    expect(screen.getByTestId('nextPlayer')).not.toHaveClass('Player-R');
+  });
+
+  it('fills the lowest free cell when click on column', () => {
+    const column = screen.getAllByTestId('column')[3];
+    column.click();
+    expect(getAllByTestId(column, 'slot')[0]).toHaveClass('Player-Y');
+
+    column.click();
+    expect(getAllByTestId(column, 'slot')[0]).toHaveClass('Player-Y');
+    expect(getAllByTestId(column, 'slot')[1]).toHaveClass('Player-R');
+  });
+
+  it('does nothing when click on a full column', () => {
+    Array.from({length: 7}).forEach(() => {
+      screen.getAllByTestId('column')[3].click();
+    });
+
+    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+    screen.getAllByTestId('column')[3].click();
+    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+  });
+
+  it ('highlights the possible play when hover', () => {
+    const column = screen.getAllByTestId('column')[3];
+    const slots = getAllByTestId(column, 'slot');
+
+    expect(slots[0]).toHaveClass('FirstFree');
+    expect(slots[1]).not.toHaveClass('FirstFree');
+
+    column.click();
+    expect(slots[1]).toHaveClass('FirstFree');
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useMemo, useState} from "react";
 import './App.css';
 
-function App() {
+type Player = 'R' | 'Y';
+type Piece = Player | '';
+
+const Slot = ({piece, firstFree}: {firstFree: boolean; piece: Piece}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="slot"
+         className={`Slot ${piece !== '' ? `Player-${piece}` : ''} ${firstFree && 'FirstFree'}`}
+    />
+  );
+};
+
+const Column = ({pieces, onClick}: {pieces: Piece[]; onClick: () => void}) => {
+  const lowestFreeIndex = useMemo(() => pieces.findIndex(slot => slot === ''),
+  [pieces]);
+
+  return (
+    <div data-testid={"column"}
+         className="Column"
+         onClick={onClick}
+    >
+      {pieces.map((cell, i) =>
+        <Slot key={i}
+              piece={cell}
+              firstFree={i === lowestFreeIndex}
+      />)}
+    </div>
+  );
+};
+
+const Board = ({onClick, pieces}: {pieces: Piece[][]; onClick: (colNum: number) => void}) => {
+  return (
+    <div data-testid="board"
+         className="Board"
+    >
+      {pieces.map((columnOfPieces, i) =>
+        <Column key={i}
+          onClick={() => onClick(i)}
+          pieces={columnOfPieces}
+        />)}
+    </div>
+  );
+};
+
+
+const NextPlayer = ({player}: {player: Player}) => {
+  return (
+    <div data-testid="nextPlayer"
+         className={`Slot NextPlayer Player-${player}`}
+    />
+  );
+};
+
+function App() {
+  const [player, setPlayer] = useState('Y' as Player);
+
+  const [pieces, setPieces] = useState(
+    Array(7).fill(
+      Array(6).fill('')
+    ) as Piece[][]
+  );
+
+  const lowestFreeSlotInCol = (colNum: number) =>
+    pieces[colNum].findIndex(piece => piece === '');
+
+  const onClickColumn = (colNum: number) => {
+    const floorNum = lowestFreeSlotInCol(colNum);
+    if (floorNum === -1) {
+      return;
+    }
+    setPieces(pieces.map((colOfPieces, col) => col !== colNum ? colOfPieces :
+      colOfPieces.map((piece, floor) => floor !== floorNum ? piece : player)));
+    setPlayer(player === 'R' ? 'Y' : 'R');
+  };
+
+  return (
+    <div className={`App Player-${player}`}>
+      <NextPlayer player={player} />
+      <Board onClick={onClickColumn} pieces={pieces} />
     </div>
   );
 }
