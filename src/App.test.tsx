4d16c925e0d6b00342bbc73cb664e351b25570aa
Gabriel Zerbib <gabriel.zerbib@gmail.com>
2022-03-29 15:11:11 +0300

    yarn

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,74 +1,76 @@
 import React from 'react';
-import {getAllByTestId, render, screen} from "@testing-library/react";
+import {render, screen, within} from "@testing-library/react";
 import App from './App';
 
-describe('connect4',  () => {
-  beforeEach(() => {
-    render(<App />);
-    expect.hasAssertions();
+it('renders game board', () => {
+  render(<App />);
+  // expect(screen.getByTestId('board')).toBeInTheDocument();
+});
+
+it('has 7 columns', () => {
+  render(<App />);
+  // expect(screen.getAllByTestId('column').length).toBe(7);
+});
+
+it('has 6 slots in every column', () => {
+  render(<App />);
+  const columns = screen.getAllByTestId('column');
+
+  columns.forEach(column => {
+    const slots = within(column).getAllByTestId('slot');
+    expect(slots.length).toBe(6);
   });
+});
 
-  it('renders game board', () => {
-    expect(screen.getByTestId('board')).toBeInTheDocument();
-  });
+it('has a NextPlayer visual, starting Yellow', () => {
+  render(<App />);
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+});
 
-  it('has 7 columns', () => {
-    expect(screen.getAllByTestId('column').length).toBe(7);
-  });
+it('changes current player when click in free place', () => {
+  render(<App />);
+  screen.getAllByTestId('column')[0].click();
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-R');
+  expect(screen.getByTestId('nextPlayer')).not.toHaveClass('Player-Y');
 
-  it('has 6 slots in every column', () => {
-    const columns = screen.getAllByTestId('column');
-    columns.forEach(column => {
-      const slots = getAllByTestId(column, 'slot');
-      expect(slots.length).toBe(6);
-    });
-  });
+  screen.getAllByTestId('column')[0].click();
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+  expect(screen.getByTestId('nextPlayer')).not.toHaveClass('Player-R');
+});
 
-  it('has a NextPlayer visual, starting Yellow', () => {
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
-  });
+it('fills the lowest free cell when click on column', () => {
+  render(<App />);
+  const column = screen.getAllByTestId('column')[3];
+  column.click();
+  expect(within(column).getAllByTestId('slot')[0]).toHaveClass('Player-Y');
 
-  it('changes current player when click in free place', () => {
-    screen.getAllByTestId('column')[0].click();
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-R');
-    expect(screen.getByTestId('nextPlayer')).not.toHaveClass('Player-Y');
+  column.click();
+  expect(within(column).getAllByTestId('slot')[0]).toHaveClass('Player-Y');
+  expect(within(column).getAllByTestId('slot')[1]).toHaveClass('Player-R');
+});
 
-    screen.getAllByTestId('column')[0].click();
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
-    expect(screen.getByTestId('nextPlayer')).not.toHaveClass('Player-R');
-  });
-
-  it('fills the lowest free cell when click on column', () => {
-    const column = screen.getAllByTestId('column')[3];
-    column.click();
-    expect(getAllByTestId(column, 'slot')[0]).toHaveClass('Player-Y');
-
-    column.click();
-    expect(getAllByTestId(column, 'slot')[0]).toHaveClass('Player-Y');
-    expect(getAllByTestId(column, 'slot')[1]).toHaveClass('Player-R');
-  });
-
-  it('does nothing when click on a full column', () => {
-    Array.from({length: 7}).forEach(() => {
-      screen.getAllByTestId('column')[3].click();
-    });
-
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+it('does nothing when click on a full column', () => {
+  render(<App />);
+  Array.from({length: 7}).forEach(() => {
     screen.getAllByTestId('column')[3].click();
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
   });
 
-  it('clears the game when Restart', () => {
-    const column = screen.getAllByTestId('column')[3];
-    const slots = getAllByTestId(column, 'slot');
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
-    column.click();
-    expect(slots[0]).not.toHaveClass('Free');
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-R');
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+  screen.getAllByTestId('column')[3].click();
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+});
 
-    screen.getByTestId('restart').click();
-    expect(slots[0]).toHaveClass('Free');
+it('clears the game when Restart', () => {
+  render(<App />);
+  const column = screen.getAllByTestId('column')[3];
+  const slots = within(column).getAllByTestId('slot');
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
+  column.click();
+  expect(slots[0]).not.toHaveClass('Free');
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-R');
 
-    expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
-  });
+  screen.getByTestId('restart').click();
+  expect(slots[0]).toHaveClass('Free');
+
+  expect(screen.getByTestId('nextPlayer')).toHaveClass('Player-Y');
 });
